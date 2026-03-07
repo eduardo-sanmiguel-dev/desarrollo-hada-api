04891245fd080714b157049dfdd2582eefb182ab
Eduardo San Miguel Domínguez García <atnasetreum@gmail.com>
2026-03-07 09:21:43 -0600

    feat: deploy

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('MAIN');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const PORT = Number(process.env.PORT) || 4002;
+  const NODE_ENV = process.env.NODE_ENV || 'development';
+  await app.listen(PORT);
+
+  logger.debug(
+    `\n\nRunning on\n\nPort: [${PORT}]\n\nenvironment: [${NODE_ENV}]\n\nTimezone: [${Intl.DateTimeFormat().resolvedOptions().timeZone}]`,
+  );
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.error('Error during application bootstrap:', err);
+  process.exit(1);
+});
